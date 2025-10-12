5a58a85fc1054a457b78fee0d334dad468afbb6f
Santiago8333 <91435566+Santiago8333@users.noreply.github.com>
2025-10-12 20:10:30 -0300

    v0.0.0

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace gestion_lotes.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
